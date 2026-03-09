0384ed019c4819b221396e90197ad354f1f91870
saaa eldeeb <saaaeldeeb@gmail.com>
2026-03-09 06:28:07 +0200

    Clean up temporary files

--- copy-img.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-fs.copyFileSync('d:/saaa eldeeb/WhatsApp Image 2026-03-05 at 1.04.54 AM.jpeg', 'd:/saaa eldeeb/public/IMD.png');
-console.log('Image copied!');
-
--- copy.ps1
+++
@@ -1,2 +0,0 @@
-Copy-Item -Path "d:\saaa eldeeb\WhatsApp Image 2026-03-05 at 1.04.54 AM.jpeg" -Destination "d:\saaa eldeeb\public\IMD.png" -Force
-
