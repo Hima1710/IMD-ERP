3f53d980499520c615281ce3bea2bad3a363146c
BLACKBOXAI <blackboxai@example.com>
2026-03-15 15:01:24 +0200

    Fix: resolve orientation type error in resizable.tsx (v2 shadcn code)

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -1,79 +1,47 @@
-'use client'
+"use client"
 
 import * as React from "react"
 import { GripVertical } from "lucide-react"
-
 import * as ResizablePrimitive from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
-const ResizablePanelGroup = React.forwardRef<
-  React.ElementRef<typeof ResizablePrimitive.PanelGroup>,
-  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelGroup>
->(({ className, ...props }, ref) => (
+const ResizablePanelGroup = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
   <ResizablePrimitive.PanelGroup
-    ref={ref}
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
     )}
     {...props}
   />
-))
-ResizablePanelGroup.displayName = ResizablePrimitive.PanelGroup.displayName
+)
 
-const ResizablePanel = React.forwardRef<
-  React.ElementRef<typeof ResizablePrimitive.Panel>,
-  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.Panel>
->(({ className, ...props }, ref) => (
-  <ResizablePrimitive.Panel
-    ref={ref}
-    className={cn(
-      "relative flex w-full grow overflow-hidden data-[panel-group-direction=vertical]:flex-col data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=horizontal]:flex-row",
-      className
-    )}
-    {...props}
-  />
-))
-ResizablePanel.displayName = ResizablePrimitive.Panel.displayName
+const ResizablePanel = ResizablePrimitive.Panel
 
-const ResizablePanelResizeHandle = React.forwardRef<
+const ResizableHandle = React.forwardRef<
   React.ElementRef<typeof ResizablePrimitive.PanelResizeHandle>,
-  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelResizeHandle>
->(({ className, orientation = "vertical", ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelResizeHandle> & {
+    withHandle?: boolean
+  }
+>(({ withHandle, className, ...props }, ref) => (
   <ResizablePrimitive.PanelResizeHandle
     ref={ref}
     className={cn(
-      "flex touch-none select-none items-center justify-center p-1 data-[panel-resize-handle-direction=horizontal]:w-[2px] data-[panel-resize-handle-direction=horizontal]:h-4 data-[panel-resize-handle-direction=vertical]:h-[2px] data-[panel-resize-handle-direction=vertical]:w-4 data-[panel-resize-handle-direction=vertical]:rotate-90",
+      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
     )}
-    orientation={orientation}
     {...props}
   >
-    <div className="w-2 h-2 bg-border rounded-full" />
+    {withHandle && (
+      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
+        <GripVertical className="h-2.5 w-2.5" />
+      </div>
+    )}
   </ResizablePrimitive.PanelResizeHandle>
 ))
-ResizablePanelResizeHandle.displayName =
-  ResizablePrimitive.PanelResizeHandle.displayName
+ResizableHandle.displayName = ResizablePrimitive.PanelResizeHandle.displayName
 
-const ResizableHandle = ({
-  className,
-  orientation = "vertical",
-  ...props
-}: React.ComponentPropsWithoutRef<typeof ResizablePanelResizeHandle> & {
-  orientation?: "horizontal" | "vertical"
-}) => (
-  <ResizablePanelResizeHandle
-    className={cn(
-      "touch-none select-none flex h-px w-full items-center justify-center bg-border",
-      '[data-orientation=vertical]:h-full [data-orientation=vertical]:w-px',
-      className
-    )}
-    orientation={orientation}
-    {...props}
-  >
-    <GripVertical className="h-3 w-3" />
-  </ResizablePanelResizeHandle>
-)
-
-export { ResizablePanel, ResizablePanelGroup, ResizableHandle }
+export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
